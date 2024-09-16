b570b61bd9b7b7f5ef58b154ccceb04aefed5994
crossdark <liuhanbo333@icloud.com>
2024-09-16 23:06:18 +0800

    0.8.0 pypi init

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CrossDown",
+    version="0.8.0",
+    author="CrossDark",
+    author_email="liuhanbo333@icloud.com",
+    description="CrossDark's MarkDown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CrossDark/CrossDown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'markdown',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent"
+    ],
+)
